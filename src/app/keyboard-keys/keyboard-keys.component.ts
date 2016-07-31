7e5d78b7712d348933f517e1a11d2d6952ce7d86
Matti Salokangas <matti.salokangas@gmail.com>
2016-07-30 19:14:58 -0700

    Final polish

---
+++ src/app/keyboard-keys/keyboard-keys.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'app-keyboard-keys',
+  templateUrl: 'keyboard-keys.component.html',
+  styleUrls: ['keyboard-keys.component.css']
+})
+export class KeyboardKeysComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
